f0f17db4733aade6dc5ef34eaffb1344caec6180
fishedee <306766045@qq.com>
2015-03-05 11:03:57 +0800

    增加fis框架的部署脚本

---
+++ util/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo npm install -g fis
+sudo npm install -g fis-parser-less
+sudo npm install -g fis-parser-sass
+sudo npm install -g fis-parser-utc
+sudo npm install -g fis-postpackager-simple
+sudo npm install -g fis-postpackager-autoload
